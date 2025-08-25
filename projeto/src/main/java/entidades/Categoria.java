34868b4a83a1c20bf629f6bbd326c438208c9c73
victor16092006 <271762@unifio.edu.br>
2025-08-25 23:47:53 +0000

    teste2

---
+++ projeto/src/main/java/entidades/Categoria.java
@@ -0,0 +1,14 @@
+package entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer codigo;
+    public String nome;
+}
