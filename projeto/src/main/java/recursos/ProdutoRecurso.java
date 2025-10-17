c017533a8e17140fa9fbaad7170afd6f44d35ba2
victor16092006 <271762@unifio.edu.br>
2025-10-17 01:25:54 +0000

    aula_16/10

---
+++ projeto/src/main/java/recursos/ProdutoRecurso.java
@@ -0,0 +1,15 @@
+package recursos;
+
+import java.util.List;
+
+import entidades.Produto;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("produtos")
+public class ProdutoRecurso {
+    @GET
+    public List<Produto> listar(){
+        return Produto.listAll();
+    }
+}
